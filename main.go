b33b6214da8277ce2d56165ec993c18a7d93a72f
bupd <bupdprasanth@gmail.com>
2024-07-26 05:31:50 +0530

    set-simple afk watcher and close the window
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	idleThreshold     = 15 * time.Second  // 5 minutes
+	notifyThreshold   = 20 * time.Second  // 2 minutes
+	checkInterval     = 5 * time.Second // Check every 10 seconds
+	notifyCommand     = "notify-send"
+	shutdownCommand   = "systemctl powerkill poweroff"
+	xprintidleCommand = "xprintidle"
+)
+
+func getIdleTime() (time.Duration, error) {
+	out, err := exec.Command(xprintidleCommand).Output()
+	if err != nil {
+		return 0, err
+	}
+	idleMilliseconds, err := strconv.ParseInt(strings.TrimSpace(string(out)), 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return time.Duration(idleMilliseconds) * time.Millisecond, nil
+}
+
+func notifyUser() error {
+	return exec.Command(notifyCommand, "AFK Alert", "You have been inactive for 5 minutes. The PC will shut down in 2 minutes if no activity is detected.").
+		Run()
+}
+
+func shutdownPC() error {
+	if err := exec.Command("powerkill").Run(); err != nil {
+		log.Fatalf("Failed while executing powerkill: %v", err)
+		return err
+	}
+	return exec.Command("systemctl", "poweroff").Run()
+}
+
+func main() {
+	for {
+		idleTime, err := getIdleTime()
+		if err != nil {
+			fmt.Println("Error getting idle time:", err)
+			time.Sleep(checkInterval)
+			continue
+		}
+    log.Println("idle time: ", idleTime)
+
+		if idleTime >= idleThreshold {
+			if err := notifyUser(); err != nil {
+				fmt.Println("Error notifying user:", err)
+				time.Sleep(checkInterval)
+				continue
+			}
+			time.Sleep(notifyThreshold)
+
+			idleTime, err = getIdleTime()
+			if err != nil {
+				fmt.Println("Error getting idle time:", err)
+				time.Sleep(checkInterval)
+				continue
+			}
+
+			if idleTime >= idleThreshold+notifyThreshold {
+				if err := shutdownPC(); err != nil {
+					fmt.Println("Error shutting down PC:", err)
+				}
+			}
+		}
+
+		time.Sleep(checkInterval)
+	}
+}
