fe27686820073ad7939011dd9ba12922c6545e2c
bupd <bupdprasanth@gmail.com>
2025-02-15 08:23:16 +0530

    update handler for database ping
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/server/handlers/handlers.go
+++ pkg/server/handlers/handlers.go
@@ -1,17 +1,27 @@
 package handlers
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"log/slog"
 	"net/http"
-
-	"github.com/bupd/digital-wellbeing/internal/database"
 )
 
 func Ping(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte("pong"))
 }
 
-func Health(dbQueries *database.Queries) http.HandlerFunc {
+func Health(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		err := db.PingContext(r.Context())
+		if err != nil {
+			slog.Error(fmt.Sprintf("error in pinging DB: %v", err))
+			_, _ = w.Write([]byte("unhealthy"))
+			return
+		}
+		slog.Debug(fmt.Sprintf("ping DB success: %v", r))
+		log.Printf("ping DB success: %v \n", r.UserAgent())
 		_, _ = w.Write([]byte("healthy"))
 	}
 }
