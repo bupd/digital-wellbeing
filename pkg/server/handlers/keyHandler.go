2bd09bffaebc0be5db7e19cd5f010a4beaf01ca2
bupd <bupdprasanth@gmail.com>
2025-02-15 10:40:01 +0530

    add list all keys from DBD
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/server/handlers/keyHandler.go
+++ pkg/server/handlers/keyHandler.go
@@ -1,1 +1,24 @@
 package handlers
+	"log"
+	"net/http"
+
+	"github.com/bupd/digital-wellbeing/internal/database"
+)
+
+func ListAllKeys(db *database.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.ListAllKeys(r.Context())
+		if err != nil {
+			log.Printf("error in listing all the keys in DB: %v", err)
+			err := &AppError{
+				Message: "Error: List All Keys Failed should not use this",
+				Code:    http.StatusNotFound,
+			}
+			HandleAppError(w, err)
+			return
+		}
+
+		log.Printf("List All Keys success, AVOID THIS, Length: %v, %v \n", len(rows), r.UserAgent())
+		WriteJSONResponse(w, http.StatusOK, rows)
+	}
+}
