74647afda9b96b433a0ef5d2ba6395316145f265
bupd <bupdprasanth@gmail.com>
2025-02-15 12:18:25 +0530

    remove unused code
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/events/process.go
+++ pkg/events/process.go
@@ -32,29 +32,23 @@ func StartHookListener(db *database.Queries) {
 	chanHook := hook.Start()
 	defer hook.End()
 
-	var keyname string
-
 	// For each event in the hook channel, process key events and add them to the database
 	for ev := range chanHook {
 		if ev.Kind == KeyDown {
-			keycode := ev.Rawcode
-			keychar := ev.Keychar
+			KeyboardPresses(ev, db)
+		} else {
+			switch ev.Kind {
+			case MouseDown:
+				mouseDown(ev, db)
+				continue
+			case MouseWheel:
+				mouseScroll(ev, db)
+				continue
+			}
+		}
+	}
+}
 
-			keyname = keymap.GetKeyName(keycode)
-			if strings.Contains(keyname, "Unknown") {
-				keyname = keymap.GetKeyName(uint16(keychar))
-			}
-			if strings.Contains(keyname, "Unknown") {
-				if ev.Rawcode > 65469 && ev.Rawcode < 65482 {
-					keyname = keymap.GetFKeyName(ev.Rawcode)
-				}
-				keyname = keymap.GetMiscKeyName(ev.Rawcode)
-			}
-
-			param := database.AddKeyParams{
-				Keyname: keyname,
-				Keycode: int64(ev.Rawcode),
-			}
 
 			// Call function to add key to database
 			row, err := db.AddKey(context.Background(), param)
