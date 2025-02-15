7030ff48ad96440d181a3f6621b18da2f841d0dc
bupd <bupdprasanth@gmail.com>
2025-02-15 08:37:28 +0530

    add add user handler
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/server/handlers/handlers.go
+++ pkg/server/handlers/handlers.go
@@ -6,7 +6,13 @@ import (
 	"log"
 	"log/slog"
 	"net/http"
+
+	"github.com/bupd/digital-wellbeing/internal/database"
 )
+
+type createUserParams struct {
+	Name string `json:"name"`
+}
 
 func Ping(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte("pong"))
@@ -23,5 +29,31 @@ func Health(db *sql.DB) http.HandlerFunc {
 		slog.Debug(fmt.Sprintf("ping DB success: %v", r))
 		log.Printf("ping DB success: %v \n", r.UserAgent())
 		_, _ = w.Write([]byte("healthy"))
+	}
+}
+
+func AddUser(db *database.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req createUserParams
+		if err := DecodeRequestBody(r, &req); err != nil {
+			log.Println(err)
+			HandleAppError(w, err)
+			return
+		}
+
+		createUserParam := database.CreateUserParams{
+			Name: req.Name,
+		}
+		row, err := db.CreateUser(r.Context(), createUserParam)
+		if err != nil {
+			log.Printf("error in creating User: %v", err)
+			err := &AppError{
+				Message: "Error: Create User Failed",
+				Code:    http.StatusInternalServerError,
+			}
+			HandleAppError(w, err)
+		}
+
+		WriteJSONResponse(w, http.StatusOK, row)
 	}
 }
