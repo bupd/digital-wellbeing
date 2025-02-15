f01de2abee9a6d4e0557b6d1e634354f35e4d8ef
bupd <bupdprasanth@gmail.com>
2025-02-15 10:42:18 +0530

    add handler for past hour
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/server/handlers/keyHandler.go
+++ pkg/server/handlers/keyHandler.go
@@ -1,4 +1,6 @@
 package handlers
+
+import (
 	"log"
 	"net/http"
 
@@ -22,3 +24,22 @@ func ListAllKeys(db *database.Queries) http.HandlerFunc {
 		WriteJSONResponse(w, http.StatusOK, rows)
 	}
 }
+
+func ListKeysPastHour(db *database.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.ListKeysPressedLastHour(r.Context())
+		if err != nil {
+			log.Printf("error in listing Past hour keys in DB: %v", err)
+			err := &AppError{
+				Message: "Error: List Past Hour Keys Failed",
+				Code:    http.StatusNotFound,
+			}
+			HandleAppError(w, err)
+			return
+		}
+
+		log.Printf("List Past Hour Keys success, Length: %v, %v \n", len(rows), r.UserAgent())
+		WriteJSONResponse(w, http.StatusOK, rows)
+	}
+}
+
