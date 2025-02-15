260ff8d33af8bc75380d6712aefb3c1ffe06cff7
bupd <bupdprasanth@gmail.com>
2025-02-15 09:00:22 +0530

    add handlers for delte user
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/server/handlers/userHandler.go
+++ pkg/server/handlers/userHandler.go
@@ -16,10 +16,7 @@ func AddUser(db *database.Queries) http.HandlerFunc {
 			return
 		}
 
-		createUserParam := database.CreateUserParams{
-			Name: req.Name,
-		}
-		row, err := db.CreateUser(r.Context(), createUserParam)
+		row, err := db.CreateUser(r.Context(), req.Name)
 		if err != nil {
 			log.Printf("error in creating User: %v", err)
 			err := &AppError{
@@ -50,5 +47,30 @@ func ListUsers(db *database.Queries) http.HandlerFunc {
 
 		log.Printf("List User success: %v \n", r.UserAgent())
 		WriteJSONResponse(w, http.StatusOK, rows)
+	}
+}
+
+func DeleteUser(db *database.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req createUserParams
+		if err := DecodeRequestBody(r, &req); err != nil {
+			log.Println(err)
+			HandleAppError(w, err)
+			return
+		}
+
+		err := db.DelteUser(r.Context(), req.Name)
+		if err != nil {
+			log.Printf("error in deleting User: %v", err)
+			err := &AppError{
+				Message: "Error: Delete User Failed",
+				Code:    http.StatusNotFound,
+			}
+			HandleAppError(w, err)
+			return
+		}
+
+		log.Printf("Delete User success: %v \n", r.UserAgent())
+		WriteJSONResponse(w, http.StatusNoContent, "{}")
 	}
 }
