70a147e70cc640235a20304fb2959dabcaef9c39
bupd <bupdprasanth@gmail.com>
2025-02-15 08:10:01 +0530

    update health method
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/server/handlers/handlers.go
+++ pkg/server/handlers/handlers.go
@@ -2,12 +2,16 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/bupd/digital-wellbeing/internal/database"
 )
 
 func Ping(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte("pong"))
 }
 
-func Health(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte("healthy"))
+func Health(dbQueries *database.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("healthy"))
+	}
 }
