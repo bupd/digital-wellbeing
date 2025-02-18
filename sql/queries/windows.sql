2942ae61c1339821525b4820196c8ebcfd6b0191
bupd <bupdprasanth@gmail.com>
2025-02-19 03:26:26 +0530

    add windows sql queries
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ sql/queries/windows.sql
@@ -0,0 +1,39 @@
+-- name: ListAllWindows :many
+SELECT * FROM windows;
+
+-- name: AddWindows :one
+INSERT OR REPLACE INTO windows (
+  wm_class,
+  is_active,
+  updated_at,
+)
+VALUES (
+  :wm_class,
+  :is_active,
+  :updated_at
+)
+RETURNING *;
+
+-- name: ListLastHourWindows :many
+SELECT *
+FROM windows
+WHERE updated_at >= datetime('now', '-1 hour');
+
+-- name: ListLastDayWindows :many
+SELECT *
+FROM windows
+WHERE created_at >= DATETIME('now', '-1 day')
+
+-- -- name: TopWinLastDay :many
+-- SELECT wm_class, wm_name, COUNT(*) as event_count
+-- FROM windows
+-- WHERE updated_at >= datetime('now', '-1 day') -- Filter for the last 24 hours
+-- GROUP BY wm_class, wm_name
+-- ORDER BY event_count DESC;
+--
+-- -- name: TopWinLastHour :many
+-- SELECT wm_class, wm_name, COUNT(*) as event_count
+-- FROM windows
+-- WHERE start_time >= datetime('now', '-1 hour') -- Filter for the last 24 hours
+-- GROUP BY wm_class
+-- ORDER BY event_count DESC;
