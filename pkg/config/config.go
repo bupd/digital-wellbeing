644648364109a3651403a558899a35bff339ae83
bupd <bupdprasanth@gmail.com>
2025-04-18 15:56:49 +0530

    update config
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/viper"
 )
@@ -30,12 +31,64 @@ func ReadConfig() (*viper.Viper, error) {
 	return v, nil
 }
 
+// Helper function to create the config file using Viper
+func CreateConfig() (*viper.Viper, error) {
+	homedir := GetHomeDir()
+	configPath := filepath.Join(homedir, ".config", "digital-wellbeing", "config.toml")
+
+	// Check if the directory exists, create it if it doesn't
+	if _, err := os.Stat(filepath.Dir(configPath)); os.IsNotExist(err) {
+		if err := os.MkdirAll(filepath.Dir(configPath), 0755); err != nil {
+			return nil, fmt.Errorf("error creating config directory: %w", err)
+		}
+	}
+
+	// Check if the file exists, create it with default config if it doesn't
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		if err := createDefaultConfigFile(configPath); err != nil {
+			return nil, fmt.Errorf("error creating config file: %w", err)
+		}
+	}
+
+	// Initialize a new Viper instance
+	v := viper.New()
+	v.SetConfigFile(configPath)
+	v.SetConfigType("toml")
+
+	// Read the configuration file
+	if err := v.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("error reading config file: %w. Please ensure the config file exists.", err)
+	}
+
+	return v, nil
+}
+
+// Helper function to create a default config file
+func createDefaultConfigFile(configPath string) error {
+	// You can customize this function to create a default TOML config file
+	// For example:
+	configContent := `# Default configuration for Digital Wellbeing
+# Add your configuration here
+PORT =  "8888"
+`
+
+	// Write the content to the file
+	if err := os.WriteFile(configPath, []byte(configContent), 0644); err != nil {
+		return fmt.Errorf("error writing default config file: %w", err)
+	}
+	return nil
+}
+
 // Reads config and gives the config
 func GetConfig() Config {
 	// Load the config file using Viper
 	v, err := ReadConfig()
 	if err != nil {
-		log.Fatalf("Error loading config: %v", err)
+		log.Printf("error loading config: %v", err)
+		v, err = CreateConfig()
+		if err != nil {
+			log.Fatalf("error creating config: %v", err)
+		}
 	}
 
 	// Map the values from Viper into the Config struct
