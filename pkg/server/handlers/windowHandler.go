e8f40b182fbfba031aa5984f54ecd16fa0e5e736
bupd <bupdprasanth@gmail.com>
2025-04-20 19:23:11 +0530

    add routes for top windows
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/server/handlers/windowHandler.go
+++ pkg/server/handlers/windowHandler.go
@@ -43,6 +43,78 @@ func ListWindowPastHour(db *database.Queries) http.HandlerFunc {
 	}
 }
 
+func TopWinLastDay(db *database.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.TopDurationWinLastDay(r.Context())
+		if err != nil {
+			log.Printf("error in listing active Window Events Duration by day in DB: %v", err)
+			err := &AppError{
+				Message: "Error: List Past Day Active Windows Failed",
+				Code:    http.StatusNotFound,
+			}
+			HandleAppError(w, err)
+			return
+		}
+
+		log.Printf("List Active Last Day Window Events success: %v, %v \n", len(rows), r.UserAgent())
+		WriteJSONResponse(w, http.StatusOK, rows)
+	}
+}
+
+func TopWinLastHour(db *database.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.TopDurationWinLastHour(r.Context())
+		if err != nil {
+			log.Printf("error in listing active Window Events Duration by hour in DB: %v", err)
+			err := &AppError{
+				Message: "Error: List Past Hour Active Windows Failed",
+				Code:    http.StatusNotFound,
+			}
+			HandleAppError(w, err)
+			return
+		}
+
+		log.Printf("List Active Last Hour Window Events success: %v, %v \n", len(rows), r.UserAgent())
+		WriteJSONResponse(w, http.StatusOK, rows)
+	}
+}
+
+func TopWinLastDayActive(db *database.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.TopActiveDurationWinLastDay(r.Context())
+		if err != nil {
+			log.Printf("error in listing active Window Events Duration by day in DB: %v", err)
+			err := &AppError{
+				Message: "Error: List Past Day Active Windows Failed",
+				Code:    http.StatusNotFound,
+			}
+			HandleAppError(w, err)
+			return
+		}
+
+		log.Printf("List Active Last Day Window Events success: %v, %v \n", len(rows), r.UserAgent())
+		WriteJSONResponse(w, http.StatusOK, rows)
+	}
+}
+
+func TopWinLastHourActive(db *database.Queries) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.TopActiveDurationWinLastHour(r.Context())
+		if err != nil {
+			log.Printf("error in listing active Window Events Duration by hour in DB: %v", err)
+			err := &AppError{
+				Message: "Error: List Past Hour Active Windows Failed",
+				Code:    http.StatusNotFound,
+			}
+			HandleAppError(w, err)
+			return
+		}
+
+		log.Printf("List Active Last Hour Window Events success: %v, %v \n", len(rows), r.UserAgent())
+		WriteJSONResponse(w, http.StatusOK, rows)
+	}
+}
+
 func ListWindowPastDay(db *database.Queries) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		rows, err := db.ListLastDayWmClass(r.Context())
