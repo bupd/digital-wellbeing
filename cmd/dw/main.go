9ce854c1572c06531b5e4784622483f01caa462b
bupd <bupdprasanth@gmail.com>
2025-04-23 20:46:54 +0530

    have daily DB
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- cmd/dw/main.go
+++ cmd/dw/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/bupd/digital-wellbeing/pkg/config"
 	"github.com/bupd/digital-wellbeing/pkg/server"
@@ -13,6 +14,13 @@ func main() {
 	conf := config.GetConfig()
 	port := conf.PORT
 	dbName := conf.DBNAME
+
+	if dbName == "daily" || len(dbName) == 0 {
+		// Format: daily-YYYY-MM-DD
+		today := time.Now().Format("2006-01-28") // user-friendly format
+		dbName = today
+		fmt.Println("Updated dbName to:", dbName)
+	}
 
 	server := server.NewServer(port, dbName)
 
